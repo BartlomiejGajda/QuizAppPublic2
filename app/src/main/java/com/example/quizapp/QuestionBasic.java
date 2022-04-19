88fd669a41fe33afbf079eb51fb4c75aea2f1058
BartlomiejGajda <bgajda@student.agh.edu.pl>
2022-04-19 12:08:45 +0200

    dynamiczna ilość przycisków-odpowiedzi

--- app/src/main/java/com/example/quizapp/QuestionBasic.java
+++ app/src/main/java/com/example/quizapp/QuestionBasic.java
@@ -1,16 +1,29 @@
 package com.example.quizapp;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class QuestionBasic {
     private String question;
     //private String answer_a, answer_b, answer_c, answer_d;
     private String[] answers;
     private String answer_correct;
-    private int counter =4;
+    private int counter;
 
     public QuestionBasic(String question, String[] answers, String answer_correct) {
         this.question = question;
         this.answers = answers;
         this.answer_correct = answer_correct;
+        this.counter = answers.length;
+
+    }
+
+    public void shuffle(){
+        List<String> list = Arrays.asList(answers);
+
+        Collections.shuffle(list);
+        list.toArray(answers);
     }
 
     public boolean checkAnswer(String answer_chosen){
--- app/src/main/java/com/example/quizapp/QuestionBasicActivity.java
+++ app/src/main/java/com/example/quizapp/QuestionBasicActivity.java
@@ -1,12 +1,10 @@
 package com.example.quizapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.widget.Button;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.view.View;
 
@@ -22,20 +20,30 @@ public class QuestionBasicActivity extends AppCompatActivity {
         setContentView(R.layout.activity_question_basic);
         Gson gson = new Gson();
         question = gson.fromJson(getIntent().getStringExtra("myjson"), QuestionBasic.class);
-
+        question.shuffle();
         TextView textView = findViewById(R.id.textViewQuestion);
-        Button[] buttons = new Button[question.getCounter()];
+        /*Button[] buttons = new Button[question.getCounter()];
         for (int i = 0; i < question.getCounter(); i++) {
             Button button = new Button(this);
             button.setId(i+1);
             button.setText("maupa");
             buttons[i] = button; }
-        ConstraintLayout ll = (ConstraintLayout) findViewById(R.id.question_basic_Layout);
+
         //LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
         for (int i = 0; i < question.getCounter(); i++) {
             ll.addView(buttons[i]);
+        }*/
+        //ConstraintLayout ll = (ConstraintLayout) findViewById(R.id.question_basic_Layout);
+        for (int i = 6; i > question.getCounter();i--){
+            int a = R.id.a6;
+            Button button = findViewById(a-i+1);
+            button.setVisibility(View.GONE);
         }
 
+        //ll.removeView(button1);
+        //ViewGroup layout = (ViewGroup) button1.getParent();
+        //if(null!=layout) //for safety only  as you are doing onClick
+        //    layout.removeView(button1);
         textView.setText(question.getQuestion());
 
 
